07b18041a1c647524c9dcfeb6a5e01bce466afa4
Sean Wilson <seancwilson@gmail.com>
2022-02-10 12:08:53 -0600

    use masonry directive for component & move options into componet file
    using the directive for the componet greatly simplifies it

--- src/v-masonry.ts
+++ src/v-masonry.ts
@@ -1,18 +1,18 @@
 import type { VueConstructor, CreateElement, VNode } from 'vue'
+import type { PropType } from 'vue/types/options'
 import Vue from 'vue'
 import Masonry from 'masonry-layout'
-import Options from './options'
-import { addClass , forEach } from './utilities'
+import masonry, { Options } from './directive.js'
+import { keys, mapKeys, mapValues, select, filter, camelcase, addClass } from './utilities.js'
 
 type VMasonry = Vue & {
-	instance: null | Masonry
 	readonly disabled: boolean
 	readonly items: any[]
-	readonly initLayout: boolean
 	readonly options: Masonry.Options
+	readonly modifiers: { [mod: string]: boolean }
 	readonly hasItems: boolean
 
-	updateInstance(disabled?: boolean): void
+	getInstance(): Masonry | null
 	renderEmpty(h: CreateElement): VNode[] | null
 	renderItems(h: CreateElement): VNode[]
 	renderStamp(h: CreateElement): VNode[]
@@ -20,62 +20,70 @@ type VMasonry = Vue & {
 	renderAppend(h: CreateElement): VNode[]
 }
 
+namespace VMasonry {
+	export const Defaults: { [key: string]: any } = {
+		itemSelector: ".v-masonry-item",
+		stamp: ".v-masonry-stamp",
+	}
+
+	export const Types: { [opt in keyof Masonry.Options]: PropType<any> } = {
+
+		// Recommended
+		itemSelector: String,
+		columnWidth: [Number, String],
+
+		// Layout
+		gutter: [Number, String],
+		horizontalOrder: Boolean,
+		percentPosition: Boolean,
+		stamp: String,
+		fitWidth: Boolean,
+		originLeft: Boolean,
+		originTop: Boolean,
+		hiddenStyle: Object,
+		visibleStyle: Object,
+
+		// Setup
+		containerStyle: Object,
+		transitionDuration: String,
+		stagger: [Number, String],
+		resize: Boolean,
+		initLayout: Boolean
+	}
+}
+
 export default (Vue as VueConstructor<VMasonry>).extend({
 
-	data() {
-		return { instance: null }
-	},
+	directives: { masonry },
 
 	props: {
-		...Options.toProps(),
+		...mapValues(VMasonry.Types, (type, name) => ({ type, default: VMasonry.Defaults[name] ?? null })),
+		...mapValues(Options.Negated, (_, name) => ({ type: Boolean, default: VMasonry.Defaults[name] ?? null })),
 		disabled: { type: Boolean, default: false },
 		items: { type: Array, default: () => [] }
 	},
 
 	computed: {
-
 		hasItems() {
 			return !!this.items.length
 		},
 
 		options() {
-			return Options.select(this)
-		}
-
-	},
-
-	watch: {
-
-		instance: {
-			handler(this: VMasonry, val: Masonry | null, old: Masonry | null) {
-				old?.allOff()
-				old?.destroy()
-				forEach({
-					layoutComplete: "layout-complete",
-					removeComplete: "remove-complete"
-				}, (emit, on) => val?.on(on, (...args) => this.$emit(emit, ...args)))
-				if (this.initLayout === false)
-					val?.layout()
-			},
-			immediate: true
+			const { disabled, $listeners } = this
+			return !disabled && {
+				on: mapKeys($listeners, camelcase),
+				...filter(select(this, keys(VMasonry.Types))),
+			} as Options
 		},
 
-		disabled: "updateInstance",
-
-		options(options: Masonry.Options) {
-			const { instance } = this
-			if (instance) {
-				instance.options = options
-				this.instance?.layout()
-			}
+		modifiers() {
+			return filter(select(this, keys(Options.Negated)))
 		}
-
 	},
 
 	methods: {
-		updateInstance(this: VMasonry, disabled: boolean = this.disabled) {
-			const { $el, options } = this
-			this.instance = disabled ? null : new Masonry($el as HTMLElement, options)
+		getInstance() {
+			return Masonry.data(this.$el) ?? null
 		},
 
 		renderEmpty(this: VMasonry, h: CreateElement): VNode[] | null {
@@ -111,26 +119,14 @@ export default (Vue as VueConstructor<VMasonry>).extend({
 	},
 
 	render(this: VMasonry, h: CreateElement) {
-		const { $scopedSlots: { default: body }, items } = this
+		const { $scopedSlots: { default: body }, items, options: value, modifiers } = this
+		const directive = { name: "masonry", value, modifiers }
 		const children = body?.({ items }) ?? [
 			...this.renderPrepend(h),
 			...(this.renderEmpty(h) ?? this.renderStamp(h)),
 			...this.renderItems(h),
 			...this.renderAppend(h)
 		]
-		return h('div', { class: 'v-masonry' }, children)
-	},
-
-	mounted(this: VMasonry) {
-		this.$nextTick(this.updateInstance)
-	},
-
-	updated() {
-		this.$nextTick(() => this.instance?.reloadItems())
-	},
-
-	destroyed(this: VMasonry) {
-		this.instance = null
+		return h('div', { class: 'v-masonry', directives: [directive] }, children)
 	}
-
 })
