a7fb26c70ee2e33690151c75aaae1715a486693f
Sean Wilson <seancwilson@gmail.com>
2022-02-10 11:53:58 -0600

    add `mapKeys` and `camelcase` utility methods

--- src/utilities.ts
+++ src/utilities.ts
@@ -2,6 +2,13 @@ import type { VNode } from 'vue'
 
 export const keys = Object.keys as <T>(o: T) => (keyof T)[]
 
+export function mapKeys<T>(obj: T, mapFn: (key: keyof T) => string) {
+	return keys(obj).reduce((mapped, key) => {
+		mapped[mapFn(key)] = obj[key]
+		return mapped
+	}, {} as Record<string, any>)
+}
+
 export function mapValues<T, R>(obj: T, mapFn: <K extends keyof T>(val: T[K], key: K, obj: T) => R) {
 	return keys(obj).reduce((mapped, key) => {
 		const value = mapFn(obj[key], key, obj)
@@ -24,6 +31,10 @@ export function invert<T extends { [key: string]: string }>(obj: T) {
 
 export function forEach<T>(obj: T, fn: <K extends keyof T>(val: T[K], key: K, obj: T) => void) {
 	keys(obj).forEach(key => fn(obj[key], key, obj))
+}
+
+export function camelcase(str: string) {
+	return str.replace(/[-\s]([a-z])/g, match => match[1].toUpperCase())
 }
 
 export function addClass(node: VNode, cls: string) {
