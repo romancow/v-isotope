9124569b66db679ec9189ed74f3ce9317b0cc0b9
Sean Wilson <seancwilson@gmail.com>
2022-04-19 17:40:09 -0500

    try triple slash reference

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+/// <reference path="../masonry-layout/index.d.ts" />
 export { default as VMasonry }  from './component.js'
 export { default as masonry } from './directive.js'
 export type { default as Masonry } from 'masonry-layout'
